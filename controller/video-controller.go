2cd42c462eafeadbc8511206363213017695e984
kmohd7121 <65274900+kmohd7121@users.noreply.github.com>
2021-01-08 16:16:51 +0530

    Add files via upload

--- controller/video-controller.go
+++ controller/video-controller.go
@@ -1,1 +1,58 @@
+package controller
 
+import (
+	"net/http"
+
+	"gilab.com/kmohd7121/gin-gonic/entity"
+	"gilab.com/kmohd7121/gin-gonic/service"
+	"gilab.com/kmohd7121/gin-gonic/validators"
+	"github.com/gin-gonic/gin"
+	"gopkg.in/go-playground/validator.v9"
+)
+
+type VideoController interface {
+	FindAll() []entity.Video
+	Save(ctx *gin.Context) error
+	ShowAll(ctx *gin.Context)
+}
+
+type controller struct {
+	service service.VideoService
+}
+
+var validate *validator.Validate
+
+func New(service service.VideoService) VideoController {
+	validate = validator.New()
+	validate.RegisterValidation("is-cool", validators.ValidateCoolTitle)
+	return &controller{
+		service: service,
+	}
+}
+
+func (c *controller) FindAll() []entity.Video {
+	return c.service.FindAll()
+}
+
+func (c *controller) Save(ctx *gin.Context) error {
+	var video entity.Video
+	err := ctx.ShouldBindJSON(&video)
+	if err != nil {
+		return err
+	}
+	err = validate.Struct(video)
+	if err != nil {
+		return err
+	}
+	c.service.Save(video)
+	return nil
+}
+
+func (c *controller) ShowAll(ctx *gin.Context) {
+	videos := c.service.FindAll()
+	data := gin.H{
+		"title":  "Video Page",
+		"videos": videos,
+	}
+	ctx.HTML(http.StatusOK, "index.html", data)
+}
