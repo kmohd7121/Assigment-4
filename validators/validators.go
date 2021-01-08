ce01b8e6a1e63009835ab0ac0e8c940c2c341a4e
kmohd7121 <65274900+kmohd7121@users.noreply.github.com>
2021-01-08 16:58:08 +0530

    Add files via upload

--- validators/validators.go
+++ validators/validators.go
@@ -1,1 +1,11 @@
+package validators
 
+import (
+	"strings"
+
+	"gopkg.in/go-playground/validator.v9"
+)
+
+func ValidateCoolTitle(field validator.FieldLevel) bool {
+	return strings.Contains(field.Field().String(), "Cool")
+}
