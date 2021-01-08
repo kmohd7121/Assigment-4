191221fd271d63418b0da166d7822aca6d1546d2
kmohd7121 <65274900+kmohd7121@users.noreply.github.com>
2021-01-08 16:12:19 +0530

    Add files via upload

---
+++ go.mod
@@ -0,0 +1,5 @@
+module gilab.com/kmohd7121/gin-gonic/
+
+go 1.15
+
+require github.com/gin-gonic/gin v1.6.3 // indirect
---
+++ server.go
@@ -0,0 +1,60 @@
+package main
+
+import (
+	"io"
+	"net/http"
+
+	"os"
+
+	"gilab.com/kmohd7121/gin-gonic/controller"
+	"gilab.com/kmohd7121/gin-gonic/middlewares"
+	"gilab.com/kmohd7121/gin-gonic/service"
+	"github.com/kmohd7121/gin"
+)
+
+var (
+	videoService    service.VideoService       = service.New()
+	videoController controller.VideoController = controller.New(videoService)
+)
+
+func setupLogOutput() {
+	f, _ := os.Create("gin.log")
+	gin.DefaultWriter = io.MultiWriter(f, os.Stdout)
+}
+
+func main() {
+
+	setupLogOutput()
+
+	server := gin.New()
+
+	server.Use(gin.Recovery(), middlewares.Logger())
+
+	server.Static("/css", "./templates/css")
+
+	server.LoadHTMLGlob("templates/*.html")
+
+	apiRoutes := server.Group("/api")
+	{
+		apiRoutes.GET("/videos", func(ctx *gin.Context) {
+			ctx.JSON(200, videoController.FindAll())
+		})
+
+		apiRoutes.POST("/videos", func(ctx *gin.Context) {
+			err := videoController.Save(ctx)
+			if err != nil {
+				ctx.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+			} else {
+				ctx.JSON(http.StatusOK, gin.H{"message": "Video Input is Valid!!"})
+			}
+
+		})
+	}
+
+	viewRoutes := server.Group("/view")
+	{
+		viewRoutes.GET("/videos", videoController.ShowAll)
+	}
+
+	server.Run(":8080")
+}
